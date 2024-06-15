611b3d8b17ac265ce8ffcc468017f28502e9c1d4
california96 <novicio.bryan@gmail.com>
2024-06-16 01:29:06 +0800

    [PATCH] - cleaner grouping of Swagger documentation

--- src/main/groovy/com/cmd/spenditapi/controller/UserController.groovy
+++ src/main/groovy/com/cmd/spenditapi/controller/UserController.groovy
@@ -3,15 +3,19 @@ import com.cmd.spenditapi.models.User
 import com.cmd.spenditapi.models.Views
 import com.cmd.spenditapi.services.UserService
 import com.fasterxml.jackson.annotation.JsonView
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.CompletableFuture
 
 @RestController
+@RequestMapping("/user")
+@Tag(name = "User Module", description = "Operations related to user management")
 class UserController {
     @Autowired
     private UserService userService
@@ -21,13 +25,13 @@ class UserController {
         this.userService = userService
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     @JsonView(Views.Public.class)
     CompletableFuture<User> getUser(@PathVariable("id") int userID) {
         userService.getUserById(userID)
     }
 
-    @PostMapping("/user/create")
+    @PostMapping("/create")
     @JsonView(Views.Internal.class)
     CompletableFuture<String> createUser(@RequestBody @JsonView(Views.Internal.class) User user){
         userService.createUser(user)
